e7449d2e76b8dbbed1ac71d7131f4c7ce2889d4d
M-A-N-I-K <95385759+M-A-N-I-K@users.noreply.github.com>
2022-07-16 23:11:02 +0530

    Shallow copy and Deep copy with copy constructor

--- Data structures in C++/OOPS 2/Code/student.cpp
+++ Data structures in C++/OOPS 2/Code/student.cpp
@@ -5,9 +5,9 @@ using namespace std;
 class student
 {
     int age;
-    char *name;
 
 public:
+    char *name;
     student(int age, char *name)
     {
         this->age = age;
@@ -20,6 +20,16 @@ public:
         strcpy(this->name, name);
     }
 
+    student(student const &s)
+    {
+        this->age = s.age;
+        // this -> name = s.name;  // Shallow copy
+
+        // Deep Copy
+        this->name = new char[strlen(s.name) + 1];
+        strcpy(this->name, s.name);
+    }
+
     void display()
     {
         cout << name << " " << age << endl;
@@ -30,10 +40,18 @@ int main()
 {
     char name[] = "abcd";
     student s1(24, name);
+    s1.display();
+
+    student s2(s1);
+    s2.name[0] = 'x';
+    s2.display();
+    s1.display();
+    /*
     name[3] = 'e';
     student s2(20, name);
     s1.display();
     s2.display();
     s1.display();
+    */
     return 0;
 }
