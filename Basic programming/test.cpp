63d069fb3386641e0acbf897eeb57eb6c72e537d
M-A-N-I-K <95385759+M-A-N-I-K@users.noreply.github.com>
2022-07-17 18:22:38 +0530

    Static member and OOPS 2 Notes added

--- Basic programming/test.cpp
+++ Basic programming/test.cpp
@@ -5,17 +5,16 @@ class Student
 {
 public:
     int rollNumber;
-    int age;
+
+    static int getRollNumber()
+    {
+        return rollNumber;
+    }
 };
 
 int main()
 {
-    Student s1;
-
-    Student const s2 = s1;
-
-    s1.rollNumber = 101;
-    s1.age = 20;
-
-    cout << s2.rollNumber << " " << s2.age;
+    Student s;
+    s.rollNumber = 101;
+    cout << s.getRollNumber() << endl;
 }
---
+++ Data structures in C++/OOPS 2/Code/student3_static_members.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class student
+{
+public:
+    int rollNumber;
+    int age;
+    static int totalStudents; // Total number of students
+};
+
+int student ::totalStudents = 0; // Initializing static data members
+
+int main()
+{
+    student s1;
+    cout << s1.rollNumber << " " << s1.age << endl;
+    cout << student ::totalStudents << endl;
+    return 0;
+}
