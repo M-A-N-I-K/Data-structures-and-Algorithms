abce48f2496cf45a112619b1aa3b0ada065890b3
M-A-N-I-K <95385759+M-A-N-I-K@users.noreply.github.com>
2022-07-22 17:01:56 +0530

    Modified polynomial class

--- Data structures in C++/OOPS 2/Code/polynomial_class.cpp
+++ Data structures in C++/OOPS 2/Code/polynomial_class.cpp
@@ -7,103 +7,135 @@ class Polynomial
 public:
     // Complete the class
     int *degCoeff; // Name of your array (Don't change this)
-    int nextIndex;
     int capacity;
 
     Polynomial()
     {
-        degCoeff = new int(5);
-        nextIndex = 0;
+        degCoeff = new int[5];
         capacity = 5;
+        for (int i = 0; i < capacity; i++)
+        {
+            degCoeff[i] = 0;
+        }
     }
-
+    Polynomial(int capacity)
+    {
+        degCoeff = new int[capacity];
+        this->capacity = capacity;
+        for (int i = 0; i < this->capacity; i++)
+        {
+            degCoeff[i] = 0;
+        }
+    }
     Polynomial(Polynomial const &p)
     {
-        degCoeff = new int[p.capacity];
-        for (int i = 0; i < p.nextIndex; i++)
+        this->degCoeff = new int[p.capacity];
+        for (int i = 0; i < p.capacity; i++)
         {
-            degCoeff[i] = p.degCoeff[i];
+            this->degCoeff[i] = p.degCoeff[i];
         }
-        nextIndex = p.nextIndex;
-        capacity = p.capacity;
+        this->capacity = p.capacity;
+    }
+    void operator=(Polynomial const &p)
+    {
+        this->degCoeff = new int[p.capacity];
+        for (int i = 0; i < p.capacity; i++)
+        {
+            this->degCoeff[i] = p.degCoeff[i];
+        }
+        this->capacity = p.capacity;
     }
 
-    void setCoefficient(int degree, int coeff)
+    void print() const
     {
-        if (degree > capacity)
+        for (int i = 0; i < capacity; i++)
         {
-            int *newDegCoeff = new int[2 * capacity];
+            if (degCoeff[i] != 0)
+            {
+                cout << degCoeff[i] << "x" << i << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    void setCoefficient(int degree, int coefficient)
+    {
+        while (degree >= capacity)
+        {
+            int *newDegCoeff = new int[capacity * 2];
             for (int i = 0; i < capacity; i++)
             {
                 newDegCoeff[i] = degCoeff[i];
             }
+            for (int i = capacity; i < 2 * capacity; i++)
+            {
+                newDegCoeff[i] = 0;
+            }
             delete[] degCoeff;
             degCoeff = newDegCoeff;
-            capacity = 2 * capacity;
+            capacity *= 2;
         }
-        degCoeff[nextIndex] = coeff;
-        nextIndex++;
+        degCoeff[degree] = coefficient;
     }
-
-    Polynomial operator+(Polynomial const &p)
+    int *getDegCoeff()
     {
-        Polynomial New;
-        for (int i = 0; i < nextIndex; i++)
-        {
-            New.degCoeff[i] = degCoeff[i] + p.degCoeff[i];
-        }
-        return New;
+        return degCoeff;
     }
-
-    Polynomial operator-(Polynomial const &p)
+    Polynomial operator+(Polynomial const &p) const
     {
-        Polynomial New;
-        for (int i = 0; i < nextIndex; i++)
+        int maxCap = (this->capacity > p.capacity) ? (this->capacity) : (p.capacity);
+        Polynomial newP(maxCap);
+        for (int i = 0; i < maxCap; i++)
         {
-            New.degCoeff[i] = degCoeff[i] - p.degCoeff[i];
-        }
-        return New;
-    }
-    Polynomial operator*(Polynomial const &p)
-    {
-        Polynomial New;
-        for (int i = 0; i < nextIndex; i++)
-        {
-            New.degCoeff[i] = degCoeff[i] * p.degCoeff[i];
-        }
-        return New;
-    }
-
-    void operator=(Polynomial const &p)
-    {
-        degCoeff = new int[p.capacity];
-        for (int i = 0; i < p.nextIndex; i++)
-        {
-            degCoeff[i] = p.degCoeff[i];
-        }
-        nextIndex = p.nextIndex;
-        capacity = p.capacity;
-    }
-
-    bool operator==(Polynomial const &p)
-    {
-        for (int i = 0; i < nextIndex; i++)
-        {
-            if (degCoeff[i] != p.degCoeff[i])
+            if (i < this->capacity && i < p.capacity)
             {
-                return false;
+                newP.degCoeff[i] = this->degCoeff[i] + p.degCoeff[i];
+            }
+            else if (i < this->capacity)
+            {
+                newP.degCoeff[i] = this->degCoeff[i];
+            }
+            else
+            {
+                newP.degCoeff[i] = p.degCoeff[i];
             }
         }
-        return true;
+        return newP;
+    }
+    Polynomial operator-(Polynomial const &p) const
+    {
+        int maxCap = (this->capacity > p.capacity) ? (this->capacity) : (p.capacity);
+        Polynomial newP(maxCap);
+        for (int i = 0; i < maxCap; i++)
+        {
+            if (i < this->capacity && i < p.capacity)
+            {
+                newP.degCoeff[i] = this->degCoeff[i] - p.degCoeff[i];
+            }
+            else if (i < this->capacity)
+            {
+                newP.degCoeff[i] = this->degCoeff[i];
+            }
+            else
+            {
+                newP.degCoeff[i] = -1 * p.degCoeff[i];
+            }
+        }
+        return newP;
     }
 
-    void print()
+    Polynomial operator*(Polynomial const &p) const
     {
-        for (int i = 0; i < nextIndex; i++)
+        int newCap = this->capacity + p.capacity;
+        Polynomial newP(newCap);
+        for (int i = 0; i < this->capacity; i++)
         {
-            cout << degCoeff[i] << "x" << i << " ";
+            for (int j = 0; j < p.capacity; j++)
+            {
+                newP.degCoeff[i + j] += this->degCoeff[i] * p.degCoeff[j];
+            }
         }
-        cout << endl;
+        return newP;
     }
 };
 
