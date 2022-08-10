83c3a966564c9abf31e5a8b296eba5bd1f561d6a
M-A-N-I-K <95385759+M-A-N-I-K@users.noreply.github.com>
2022-08-10 12:49:15 +0530

    Take input level Wise Added

--- Data structures in C++/Binary Trees/Code/BinaryTree.cpp
+++ Data structures in C++/Binary Trees/Code/BinaryTree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
 template <typename T>
@@ -60,6 +61,44 @@ binaryTreeNode<int> *takeInput()
     return root;
 }
 
+binaryTreeNode<int> *takeInputLevelWise()
+{
+    int rootData;
+    cout << "Enter root Data :" << endl;
+    cin >> rootData;
+    if (rootData == -1)
+    {
+        return NULL;
+    }
+    binaryTreeNode<int> *root = new binaryTreeNode<int>(rootData);
+    queue<binaryTreeNode<int> *> pendingNodes;
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0)
+    {
+        binaryTreeNode<int> *front = pendingNodes.front();
+        pendingNodes.pop();
+        cout << "Enter left child of " << front->data << endl;
+        int leftChildData;
+        cin >> leftChildData;
+        if (leftChildData != -1)
+        {
+            binaryTreeNode<int> *leftChild = new binaryTreeNode<int>(leftChildData);
+            front->left = leftChild;
+            pendingNodes.push(leftChild);
+        }
+        cout << "Enter right child of " << front->data << endl;
+        int rightChildData;
+        cin >> rightChildData;
+        if (rightChildData != -1)
+        {
+            binaryTreeNode<int> *rightChild = new binaryTreeNode<int>(rightChildData);
+            front->right = rightChild;
+            pendingNodes.push(rightChild);
+        }
+    }
+    return root;
+}
+
 int main()
 {
     /*
@@ -69,7 +108,8 @@ int main()
     root->left = node1;
     root->right = node2;
     */
-    binaryTreeNode<int> *root = takeInput();
+    // binaryTreeNode<int> *root = takeInput();
+    binaryTreeNode<int> *root = takeInputLevelWise();
     printTree(root);
     delete root;
     return 0;
