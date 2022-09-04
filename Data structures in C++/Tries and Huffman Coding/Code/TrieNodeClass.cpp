34a1854c4389deb19f2f1abc32e169c2c789a496
Manik Dingra <manikdhingra0582@gmail.com>
2022-09-04 23:43:37 +0530

    Trie Class Remove function added

--- Data structures in C++/Tries and Huffman Coding/Code/TrieNodeClass.cpp
+++ Data structures in C++/Tries and Huffman Coding/Code/TrieNodeClass.cpp
@@ -56,13 +56,84 @@ private:
         insertWord(child, word.substr(1));
     }
 
+    bool search(TrieNode *root, string word)
+    {
+        if (word.length() == 0)
+        {
+            return root->isTerminal && true;
+        }
+        int index = word[0] - 'a';
+        TrieNode *child;
+        if (root->children[index] != NULL)
+        {
+            child = root->children[index];
+        }
+        else
+        {
+            return root->isTerminal && false;
+        }
+        bool ans = search(child, word.substr(1));
+        return ans;
+    }
+    void removeWord(TrieNode *root, string word)
+    {
+        if (word.size() == 0)
+        {
+            root->isTerminal = false;
+            return;
+        }
+        int index = word[0] - 'a';
+        TrieNode *child;
+        if (root->children[index] != NULL)
+        {
+            child = root->children[index];
+        }
+        else
+        {
+            // Word not found
+            return;
+        }
+        removeWord(child, word.substr(1));
+
+        // Remove Child Node if it is useless
+        if (child->isTerminal == false)
+        {
+            for (int i = 0; i < 26; i++)
+            {
+                if (child->children[i] != NULL)
+                {
+                    return;
+                }
+            }
+            delete child;
+            root->children[index] = NULL;
+        }
+    }
+
 public:
     void insertWord(string word)
     {
         insertWord(root, word);
     }
+
+    bool search(string word)
+    {
+        return search(root, word);
+    }
+
+    void removeWord(string word)
+    {
+        removeWord(root, word);
+    }
 };
 
 int main()
 {
+    Trie t;
+    t.insertWord("and");
+    t.insertWord("dot");
+    t.insertWord("double");
+    cout << t.search("and") << endl;
+    t.removeWord("and");
+    cout << t.search("and") << endl;
 }
