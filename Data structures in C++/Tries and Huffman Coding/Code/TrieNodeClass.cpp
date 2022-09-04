1897b80c6c8c4d93eca3233d2d856b295f915e52
Manik Dingra <manikdhingra0582@gmail.com>
2022-09-04 22:56:33 +0530

    Trie Class Insert function added

--- Data structures in C++/Tries and Huffman Coding/Code/TrieNodeClass.cpp
+++ Data structures in C++/Tries and Huffman Coding/Code/TrieNodeClass.cpp
@@ -13,7 +13,53 @@ public:
     {
         this->data = data;
         children = new TrieNode *[26];
+        for (int i = 0; i < 26; i++)
+        {
+            children[i] = NULL;
+        }
         isTerminal = false;
+    }
+};
+
+class Trie
+{
+    TrieNode *root;
+
+public:
+    Trie()
+    {
+        root = new TrieNode('\0');
+    }
+
+private:
+    void insertWord(TrieNode *root, string word)
+    {
+        // Base Case
+        if (word.length() == 0)
+        {
+            root->isTerminal = true;
+            return;
+        }
+        // Small Calculation
+        int index = word[0] - 'a';
+        TrieNode *child;
+        if (root->children[index] != NULL)
+        {
+            child = root->children[index];
+        }
+        else
+        {
+            child = new TrieNode(word[0]);
+            root->children[index] = child;
+        }
+        // Recursive Call
+        insertWord(child, word.substr(1));
+    }
+
+public:
+    void insertWord(string word)
+    {
+        insertWord(root, word);
     }
 };
 
