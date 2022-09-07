832fc5dceb783aab46470e6a28daebb8788efe36
Manik Dingra <manikdhingra0582@gmail.com>
2022-09-07 18:34:47 +0530

    Min steps to one DP Approach Added

--- Data structures in C++/DP/Code/MinStepsToOne.cpp
+++ Data structures in C++/DP/Code/MinStepsToOne.cpp
@@ -117,6 +117,27 @@ int countStepsToOne2(int n)
 }
 
 // Dynamic Programming
+int countStepsToOne3(int n)
+{
+    int *ans = new int[n + 1];
+    ans[0] = 0;
+    ans[1] = 0;
+    for (int i = 2; i <= n; i++)
+    {
+        int x = INT_MAX, y = INT_MAX, z = INT_MAX;
+        x = ans[i - 1] + 1;
+        if (i % 2 == 0)
+        {
+            y = ans[i / 2] + 1;
+        }
+        if (i % 3 == 0)
+        {
+            z = ans[i / 3] + 1;
+        }
+        ans[i] = min(x, min(y, z));
+    }
+    return ans[n];
+}
 
 int main()
 {
