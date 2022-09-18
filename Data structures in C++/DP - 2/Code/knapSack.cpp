c39f6d31b85ac304a13f9925ade133dff6a7a204
Manik Dingra <manikdhingra0582@gmail.com>
2022-09-18 17:18:15 +0530

    Knapsack problem DP approach added

--- Data structures in C++/DP - 2/Code/knapSack.cpp
+++ Data structures in C++/DP - 2/Code/knapSack.cpp
@@ -47,6 +47,45 @@ Sample Output 2 :
 #include <iostream>
 #include <algorithm>
 using namespace std;
+// DP Approach
+int knapsackDp(int *weight, int *value, int n, int maxWeight)
+{
+    int **dp = new int *[n + 1];
+    for (int i = 0; i <= n; i++)
+    {
+        dp[i] = new int[maxWeight + 1];
+    }
+    for (int i = 0; i <= n; i++)
+    {
+        dp[i][0] = 0;
+    }
+    for (int i = 0; i <= maxWeight; i++)
+    {
+        dp[0][i] = 0;
+    }
+    for (int i = 1; i < n + 1; i++)
+    {
+        for (int j = 1; j < maxWeight + 1; j++)
+        {
+            if (weight[i - 1] <= j)
+            {
+                dp[i][j] = max(value[i - 1] + dp[i - 1][j - weight[i - 1]], dp[i - 1][j]);
+            }
+            else
+            {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+    int ans = dp[n][maxWeight];
+    for (int i = 0; i <= n; i++)
+    {
+        delete[] dp[i];
+    }
+    delete[] dp;
+    return ans;
+}
+
 // Memoization Approach
 int knapsackHelper(int *weight, int *value, int n, int maxWeight, int **arr)
 {
