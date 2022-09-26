1ec8eef0fc45164d46fb79f0ebf476d40e9b90fb
Manik Dingra <manikdhingra0582@gmail.com>
2022-09-26 14:48:58 +0530

    Types of inheritance added

---
+++ Data structures in C++/OOPs - 3/typesOfInheritance.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+class teacher
+{
+public:
+    string name;
+    int age;
+
+    void print()
+    {
+        cout << "Teacher" << endl;
+    }
+};
+
+class student
+{
+public:
+    void print()
+    {
+        cout << "Student" << endl;
+    }
+};
+
+class TA : public teacher, public student
+{
+};
+
+int main()
+{
+    TA t;
+    t.student ::print();
+}
