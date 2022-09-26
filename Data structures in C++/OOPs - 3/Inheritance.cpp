994430beb7bf0c1250c24647a6b4ff341b634a4d
Manik Dingra <manikdhingra0582@gmail.com>
2022-09-26 14:26:37 +0530

    Order of constructors called added

--- Data structures in C++/OOPs - 3/Inheritance.cpp
+++ Data structures in C++/OOPs - 3/Inheritance.cpp
@@ -12,13 +12,34 @@ protected:
 
 public:
     string color;
+    vehicle()
+    {
+        cout << "This is constructor of vehicle class" << endl;
+    }
+    vehicle(int z)
+    {
+        cout << "This is Parameterized constructor of vehicle class" << endl;
+        maxSpeed = z;
+    }
+    ~vehicle()
+    {
+        cout << "This is destructor of vehicle class" << endl;
+    }
 };
 
 class car : public vehicle
 {
 public:
     int numGears;
-
+    car(int x, int y) : vehicle(x)
+    {
+        cout << "This is constructor of car class" << endl;
+        numGears = y;
+    }
+    ~car()
+    {
+        cout << "This is destructor of car class" << endl;
+    }
     void print()
     {
         cout << "Numtyres  : " << numTyres << endl;
@@ -27,15 +48,30 @@ public:
     }
 };
 
+class hondaCity : public car
+{
+public:
+    hondaCity(int x, int y) : car(x, y)
+    {
+        cout << "Honda city constructor" << endl;
+    }
+    ~hondaCity()
+    {
+        cout << "Honda city destructor" << endl;
+    }
+};
+
 int main()
 {
-    vehicle v;
-    v.color = "Blue";
+    // vehicle v;
+    // v.color = "Blue";
     // v.maxSpeed = 100;
     // v.numTyres = 4;
 
-    car c;
-    c.color = "Red";
-    c.numGears = 6;
-    c.print();
+    // car c(10);
+    // c.color = "Red";
+    // c.numGears = 6;
+    // c.print();
+
+    hondaCity h(10, 20);
 }
