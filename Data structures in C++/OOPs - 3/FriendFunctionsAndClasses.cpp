5bb7033774972f2d851ee5a2b344e38e0995a768
Manik Dingra <manikdhingra0582@gmail.com>
2022-09-27 15:29:26 +0530

    Friend function and friend classes added

---
+++ Data structures in C++/OOPs - 3/FriendFunctionsAndClasses.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+class bus
+{
+public:
+    void print();
+};
+
+void test();
+
+class truck
+{
+private:
+    int x;
+
+protected:
+    int y;
+
+public:
+    int z;
+    friend void bus ::print();
+    friend void test();
+};
+
+void bus ::print()
+{
+    truck t;
+    t.x = 10;
+    t.y = 20;
+    cout << t.x << " " << t.y << endl;
+}
+
+void test()
+{
+    truck t;
+    t.x = 10;
+    t.y = 20;
+    cout << t.x << " " << t.y << endl;
+}
+
+int main()
+{
+    bus b;
+    b.print();
+    test();
+}
