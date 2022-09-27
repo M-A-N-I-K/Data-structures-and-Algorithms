5357cbd33a3ff76adfedaa198fe38206d652a330
Manik Dingra <manikdhingra0582@gmail.com>
2022-09-27 14:52:51 +0530

    Compiler Time Polymorphism added

---
+++ Data structures in C++/OOPs - 3/CompileTimePolymorphism.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+class vehicle
+{
+public:
+    string color;
+    void print()
+    {
+        cout << "vehicle" << endl;
+    }
+};
+
+class car : public vehicle
+{
+public:
+    int numTyres;
+    void print()
+    {
+        cout << "car" << endl;
+    }
+};
+
+int main()
+{
+    vehicle v;
+    car c;
+    v.print();
+    c.print();
+
+    vehicle *v1 = new vehicle;
+    vehicle *v2;
+    // v2 = &v;
+    v2 = &c;
+    v2->print();
+    v1->print();
+}
